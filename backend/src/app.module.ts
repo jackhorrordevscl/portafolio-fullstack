6c597ac38b4048583a5351ac6a08425b133d6dd5
Juan Martinez <jmartinezc.cp@gmail.com>
2026-04-08 21:00:53 -0400

    implementación de MailerModule para envíos de correo desde formulario de contacto

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,10 +1,62 @@
-import { Module } from "@nestjs/common";  
-import { HealthModule } from './modules/health/health.module';
+import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { MailerModule } from '@nestjs-modules/mailer';
+import { ConfigService } from '@nestjs/config';
 import { ContactModule } from './modules/contact/contact.module';
 
 @Module({
-  imports: [HealthModule, ContactModule],
-  controllers: [],
-  providers: [],
+  imports: [
+    ContactModule,
+
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+
+    MailerModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => {
+        const user = configService.get<string>('MAIL_USER');
+        const pass = configService.get<string>('MAIL_PASS');
+
+        console.log('MAIL_USER:', user);
+        console.log('MAIL_PASS:', pass ? 'OK' : 'UNDEFINED');
+
+        return {
+          transport: {
+            host: configService.get<string>('MAIL_HOST'),
+            port: Number(configService.get<number>('MAIL_PORT')),
+            secure: false, // GMAIL CON 587 -> STARTTLS
+            auth: {
+              user,
+              pass,
+            },
+          },
+          defaults: {
+            from: configService.get<string>('MAIL_FROM'),
+          },
+        };
+      },
+    }),
+  ],
 })
 export class AppModule {}
+
+/**
+ * 
+ useFactory: (configService: ConfigService) => ({
+        transport: {
+          host: configService.get<string>('MAIL_HOST'),
+          port: configService.get<number>('MAIL_PORT'),
+          secure: false, // GMAIL CON 587 -> STARTTLS
+          auth: {
+            user: configService.get<string>('MAIL_USER'),
+            pass: configService.get<string>('MAIL_PASS'),
+          },
+        },
+        defaults: {
+          from: configService.get<string>('MAIL_FROM'),
+        },
+      }),
+    }),
+ * 
+ */
--- backend/src/modules/contact/contact.service.ts
+++ backend/src/modules/contact/contact.service.ts
@@ -1,21 +1,45 @@
 import { Injectable } from "@nestjs/common";
+import { MailerService } from "@nestjs-modules/mailer";
+import { ConfigService } from "@nestjs/config";
 import { CreateContactDto } from "./dto/create-contact.dto";
+import { Subject } from "rxjs";
 
 @Injectable()
 export class ContactService {
-    async handleContact(dto: CreateContactDto) {
-        
-        console.log('📩 Nuevo Mensaje de Contacto:', {
-            name: dto.name,
-            email: dto.email,
-            subject: dto.subject,
-            message: dto.message,
-            messageLength: dto.message.length,
-        });
+    constructor(
+        private readonly mailerService: MailerService,
+        private readonly configService: ConfigService,
+    ) {}
 
-        return {
-            success: true,
-            message: 'Mensaje recibido correctamente',
-        };
+    async handleContact(dto: CreateContactDto) {
+        const { name, email, subject, message } = dto;
+        const to = this.configService.get<string>('MAIL_TO');
+
+        try {
+            await this.mailerService.sendMail({
+                to,
+                subject: `[PORTAFOLIO] ${subject}`,
+                replyTo: email,
+                text: `
+                    Nombre: ${name}
+                    Email: ${email}
+
+                    Mensaje:
+                    ${message}
+                `,
+                    html: `
+                        <h2>Nuevo mensaje desde el portafolio</h2>
+                        <p><strong>Nombre:</strong> ${name}</p>
+                        <p><strong>Email:</strong> ${email}</p>
+                        <p><strong>Mensaje:</strong></p>
+                        <p>${message}</p>
+                    `,
+            });
+            return { success: true };
+        } catch (error) {
+            //NO FILTRAR AQUI, DEJA AL INTERCEPTOR GLOBAL ACTUAR.
+            throw error;
+        }
+        
     }
 }
